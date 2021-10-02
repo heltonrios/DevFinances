d0b1aaad97ef08a22998a2b900dcd5cfc7d9598a
Helton Rios <helton.placido@gmail.com>
2021-10-02 02:46:05 -0300

    Aula 03 Js

--- main.js
+++ main.js
@@ -1,4 +1,44 @@
-Modal = {
-  open() {},
-  close() {}
+const Modal = {
+  open() {
+    document.querySelector('.modal-overlay').classList.add('active')
+  },
+  close() {
+    document.querySelector('.modal-overlay').classList.remove('active')
+  }
+}
+
+const transactions = [
+  {
+    id: 1,
+    description: 'Luz',
+    amount: -50000,
+    date: '23/01/2021'
+  },
+
+  {
+    id: 2,
+    description: 'Site',
+    amount: 500000,
+    date: '23/01/2021'
+  },
+
+  {
+    id: 3,
+    description: 'Internet',
+    amount: -20000,
+    date: '23/01/2021'
+  }
+]
+
+const Transaction = {
+  incomes() {
+    //somar as entradas
+  },
+  expenses() {
+    //somar as saidas
+  },
+
+  total() {
+    //entradas - saidas
+  }
 }
